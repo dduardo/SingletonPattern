4f5bfa377ab496e434ce98172590d31566b65028
Carlos Eduardo Santiago <deduardo_6@hotmail.com>
2017-08-27 12:50:48 -0300

    Initial Commit

---
+++ SingletonPattern/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SingletonPattern
+//
+//  Created by DuardoSantiago on 27/08/17.
+//  Copyright © 2017 DuardoSantiago. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
